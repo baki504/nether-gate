e31ebb7b36611e1292cd46cc0cf40f550dc3daaf
baki504 <baki504@users.noreply.github.com>
2024-02-18 15:30:22 +0900

    initial commit

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-player.onChat("run", function () {
-	
+player.onChat("createNetherGate", function () {
+    blocks.fill(
+    OBSIDIAN,
+    pos(-2, 0, 5),
+    pos(2, 5, 5),
+    FillOperation.Replace
+    )
+    blocks.fill(
+    AIR,
+    pos(-1, 1, 0),
+    pos(1, 4, 5),
+    FillOperation.Replace
+    )
+    blocks.place(FIRE, pos(0, 1, 5))
 })
